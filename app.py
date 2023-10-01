2a5a9b70316297f493d5f4335b262213daacc8f7
vanchha-s <vanchhasahu98@gmail.com>
2023-10-02 01:16:33 +0530

    Second commit

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 print ("This is a calculation function")
 x = input("first number ")
 y = input("second number ")
 def add(x,y):
     print(x+y)
 def subtract(x,y):
-    return x-y
+    print(x-y)
 def multiply(x,y):
-    pass
+    print(x*y)
 def divide(x,y):
-    pass
-=======
-print ("this is a python file")
-print("modified file")
-my_list = [12,3,5,10]
-count(my_list)
->>>>>>> b313c5628a16d5c359f6173bea8f52d2c587dbb0
+    print(x/y)
