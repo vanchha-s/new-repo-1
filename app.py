973aad893988975fb66c19c35db5240d133b1c09
vanchha-s <vanchhasahu98@gmail.com>
2023-10-01 23:23:03 +0530

    own commit

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-print ("this is a python file")
-print("modified file")
+print ("This is a calculation function")
+x = input("first number ")
+y = input("second number ")
+def add(x,y):
+    print(x+y)
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    pass
+def divide(x,y):
+    pass
